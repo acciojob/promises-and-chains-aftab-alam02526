a503a031bc6b50669c87240d6e1265ad8f9017f4
aftab-alam02526 <aftab0447@gmail.com>
2025-10-02 15:53:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,23 +16,6 @@ const form = document.getElementById("userForm");
       }
 
       // Create a promise
-  const form = document.getElementById("userForm");
-    const nameInput = document.getElementById("name");
-    const ageInput = document.getElementById("age");
-
-    form.addEventListener("submit", function (event) {
-      event.preventDefault(); // prevent page reload
-
-      const name = nameInput.value.trim();
-      const age = parseInt(ageInput.value);
-
-      // Validation: Empty inputs
-      if (!name || !ageInput.value) {
-        alert("Please enter valid details");
-        return;
-      }
-
-      // Create a promise
       const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
           if (age > 18) {

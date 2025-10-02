17a9a715505c2d4c791c7b616d0bf5cf1513c815
aftab-alam02526 <aftab0447@gmail.com>
2025-10-02 15:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,17 +16,40 @@ const form = document.getElementById("userForm");
       }
 
       // Create a promise
-      const promise = new Promise((resolve, reject) => {
+      <script>
+  const form = document.getElementById("userForm");
+  const nameInput = document.getElementById("name");
+  const ageInput = document.getElementById("age");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // prevent reload
+
+    const name = nameInput.value.trim();
+    const age = parseInt(ageInput.value);
+
+    if (!name || !ageInput.value) {
+      alert("Please enter valid details");
+      return;
+    }
+
+    function checkAge(name, age) {
+      return new Promise((resolve, reject) => {
         setTimeout(() => {
           if (age > 18) {
-            resolve(`Welcome, ${name}. You can vote`);
+            resolve(`Welcome, ${name}. You can vote.`);
           } else {
-            reject(`Oh sorry ${name}. You aren't old enough`);
+            reject(`Oh sorry ${name}. You aren't old enough.`);
           }
         }, 4000);
       });
+    }
 
-      promise
-        .then((message) => alert(message))
-        .catch((errorMessage) => alert(errorMessage));
-    });
+    checkAge(name, age)
+      .then((msg) => {
+        alert(msg);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  });
+
